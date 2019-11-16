f8f2b2ea8c867ee7581d161a672c007f02e6cb56
Angel Jimenez <angeljpp29@gmail.com>
2019-11-16 06:09:05 -0600

    function PUT

--- Lab03ED2/Lab03ED2/Controllers/PizzaController.cs
+++ Lab03ED2/Lab03ED2/Controllers/PizzaController.cs
@@ -16,19 +16,25 @@ namespace Lab03ED2.Controllers
 
         // GET: api/Pizza
         [HttpGet]
-        public IEnumerable<Models.Pizza> Get()
+        public IActionResult Get()
         {
             Models.Pizza aux = new Models.Pizza();
             aux.Nombre = "Pizza1";
             aux.Tamanio = "Pequenia";
             db.Listadopizzas.Add(aux);
-            return db.Listadopizzas.ToArray();
+            if (db.Listadopizzas.Count == 0)
+                return NotFound();
+            return Ok(db.Listadopizzas.ToArray());
         }
 
         // GET: api/Pizza/5
         [HttpGet("{id}", Name = "Get")]
         public IActionResult Get(int id)
         {
+            if (db.Listadopizzas.Count == 0)
+                return NotFound();
+            if (db.Listadopizzas.Count < id-1 || id < 0)
+                return NotFound();
             return Ok(db.Listadopizzas[id]);
         }
 
@@ -39,21 +45,35 @@ namespace Lab03ED2.Controllers
             if (!ModelState.IsValid)
                 return BadRequest("Tipo de dato no valido");
             if (value.Nombre == null || value.Cantidad_Porciones == 0 || value.Ingredientes == null || value.Tamanio == null || value.Tipo_Masa == null)
-                return BadRequest("Tipo de dato no valido");
+                return BadRequest("Valores no validos");
             db.Listadopizzas.Add(value);
             return Ok();
         }
 
         // PUT: api/Pizza/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Models.Pizza value)
         {
+            if (!ModelState.IsValid)
+                return BadRequest("Tipo de dato no valido");
+            if (db.Listadopizzas.Count < id - 1 || id < 0)
+                return NotFound();
+            if (value.Nombre == null || value.Cantidad_Porciones == 0 || value.Ingredientes == null || value.Tamanio == null || value.Tipo_Masa == null)
+                return BadRequest("Valores no validos");
+            db.Listadopizzas.Insert(id, value);
+            return Ok(db.Listadopizzas[id]);
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            if (db.Listadopizzas.Count == 0)
+                return NotFound();
+            if (db.Listadopizzas.Count < id - 1 || id < 0)
+                return NotFound();
+            db.Listadopizzas.RemoveAt(id);
+            return Ok();
         }
     }
 }
