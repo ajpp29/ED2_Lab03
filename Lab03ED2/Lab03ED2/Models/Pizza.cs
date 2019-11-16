e8ab07266340e26be5a5367cf31ac2b7d2fd2de8
Angel Jimenez <angeljpp29@gmail.com>
2019-11-16 02:14:23 -0600

    Create Pizza class

---
+++ Lab03ED2/Lab03ED2/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab03ED2.Models
+{
+    public class Pizza
+    {
+        string Nombre { get; set; }
+        string Descripcion { get; set; }
+        string[] Ingredientes { get; set; }
+        string Tipo_Masa { get; set; }
+        string Tamanio { get; set; }
+        int Cantidad_Porciones { get; set; }
+        bool Extra_Queso { get; set; }
+    }
+}
