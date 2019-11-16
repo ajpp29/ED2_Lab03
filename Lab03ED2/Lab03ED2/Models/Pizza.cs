c1a9ae739a61c95677e7b3cc06dedab31327c024
Angel Jimenez <angeljpp29@gmail.com>
2019-11-16 06:48:43 -0600

    Update Pizza.cs

--- Lab03ED2/Lab03ED2/Models/Pizza.cs
+++ Lab03ED2/Lab03ED2/Models/Pizza.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Lab03ED2.Models
 {
     public class Pizza
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public string[] Ingredientes { get; set; }
