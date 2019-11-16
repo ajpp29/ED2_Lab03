86cf711bd86b1643d1ab4432189cb231505c0a5e
Pablohe18 <31459420+Pablohe18@users.noreply.github.com>
2019-11-15 18:21:30 -0600

    Subiendo Serie 1

---
+++ LabApis/LaboratorioAPIs/Code/LJWT.cs
@@ -0,0 +1,43 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioAPIs.Repository
+{
+    public class LJWT
+    {
+        
+        public async Task<string> GenerarJWT(IFormFile file, string llave)
+        {
+            var lectura = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    lectura.AppendLine(await reader.ReadLineAsync()); 
+            }
+            var json = lectura.ToString();
+            var claims = await CreateClaimsIdentities(json);
+            var token = new JwtBuilder()
+              .WithAlgorithm(new HMACSHA256Algorithm())
+              .WithSecret(llave)
+              .AddClaim("userdata", json)
+              .Build();
+            await Console.Out.WriteLineAsync(token);
+
+            return token;
+        }
+
+        public static Task<ClaimsIdentity> CreateClaimsIdentities(string json)
+        {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+            claimsIdentity.AddClaim(new Claim("userdata", json));
+            return Task.FromResult(claimsIdentity);
+        }
+    }
+}
---
+++ LabApis/LaboratorioAPIs/Controllers/JWTController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LaboratorioAPIs.Repository;
+
+namespace LaboratorioAPIs.Controllers
+{
+    public class JWTController : Controller
+    {
+       
+        [HttpGet]
+        public ActionResult IndexJWT(string TOKEN)
+        {
+            if (TOKEN!= null)
+            {
+                if (TOKEN.Contains("$token$"))
+                {
+                    ViewBag.Token = TOKEN.Substring(0,TOKEN.Length-7);
+                }
+                else
+                {
+                    ViewBag.EstatusDeSubida = TOKEN;
+                }
+            }
+            
+            
+            return View();
+        }  
+
+        [HttpPost]
+        public ActionResult IndexJWT(IFormFile Archivo, string Key)
+        {
+                if (Archivo.FileName.Contains(".json"))
+                {
+                    if (Key.Length >= 16)
+                    {
+                        LJWT logicaJWT = new LJWT();
+                        var token = logicaJWT.GenerarJWT(Archivo, Key).Result;
+                        return IndexJWT(token + "$token$");
+                    }
+                    else
+                    {
+                        return IndexJWT("Key Mínima 16 caracteres");
+                    }
+                    
+                }
+                else
+                {
+                    return IndexJWT("Formato inválido");  
+                }
+        }
+     }  
+        
+
+
+    }
